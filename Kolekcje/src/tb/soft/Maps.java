8b1e0e92fa20f8d13defcf6020b676387ab4b641
dominikkilijan <dominikkilijan@gmail.com>
2021-11-05 00:44:51 +0100

    Initial Maps

---
+++ Kolekcje/src/tb/soft/Maps.java
@@ -0,0 +1,19 @@
+package tb.soft;
+
+public class Maps implements IColletionMethods{
+
+    @Override
+    public void addPersonToCollections(Person person) {
+
+    }
+
+    @Override
+    public void removePersonFromCollections(Person person) {
+
+    }
+
+    @Override
+    public void iterateCollections() {
+
+    }
+}
