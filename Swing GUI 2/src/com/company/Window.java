bb0c5c056fb15cf5fbd24771033b1e475d7d2517
dominikkilijan <dominikkilijan@gmail.com>
2021-12-01 10:54:09 +0100

    End of the classes

--- Swing GUI 2/src/com/company/Window.java
+++ Swing GUI 2/src/com/company/Window.java
@@ -1,11 +1,15 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Window extends JFrame {
     JFrame frame = new JFrame();
     JButton runButton = new JButton("Run");
     JButton cancelButton = new JButton("Cancel");
+    JPanel buttonPanel = new JPanel();
 
     Window() {
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -15,8 +19,28 @@ public class Window extends JFrame {
 
         frame.add(runButton);
         runButton.setBounds(20,400,80,25);
+        runButton.add(buttonPanel);
+        buttonPanel.setBackground(Color.GRAY);
+        buttonPanel.setLayout(new GridLayout());
+        buttonPanel.setSize(60,25);
+        buttonPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                int i;
+
+                runButton.setLocation(frame.getMousePosition());
+            }
+        });
         frame.add(cancelButton);
         cancelButton.setBounds(350,400,80,25);
+        cancelButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                runButton.setLocation(20,400);
+            }
+        });
 
         frame.setVisible(true);
     }
